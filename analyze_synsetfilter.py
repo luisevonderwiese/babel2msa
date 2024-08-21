6f2f3d0a40c05c253fa6cf315cc351cedb476a67
Luise Haeuser <haeusele@hitssv566.villa-bosch.de>
2024-08-21 09:27:32 +0000

    Quantify synset availability

--- analyze_synsetfilter.py
+++ analyze_synsetfilter.py
@@ -9,9 +9,15 @@ if not os.path.isdir(outdir):
     os.makedirs(outdir)
 
 for name in ["filter",  "filter_epitran"]:
+    print(name)
     path = os.path.join(ranking_dir, name + ".tsv")
     df = pd.read_csv(path, sep = "\t")
-    print(df["count"])
+    print("Percentage of synsets available in 20 languages or less")
+    print((len(df[df["count"] <= 20]) / len(df)) * 100)
+    print("Percentage of synsets available in more than 60 languages")
+    print((len(df[df["count"] > 60]) / len(df)) * 100)
+    print("Largest synset")
+    print(max(df["count"]))
     plt.hist(df["count"], bins = 20)
     plt.xlabel("Number of languages")
     plt.ylabel("Number of synsets")
