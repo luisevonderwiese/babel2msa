b6e7003b27ec88ee9f0d7d358fc0541c0e7e15d1
Luise Haeuser <haeusele@hitssv566.villa-bosch.de>
2025-02-04 13:35:05 +0000

    Remove lexibench wordlists as just metadata is required for entropies analysis

--- entropies_lexibench.py
+++ entropies_lexibench.py
@@ -1,53 +1,11 @@
 import os
-import json
-from cognate import CognateData
-
-
-import jpype
-import jpype.imports
-from jpype.types import *
-
-# Launch the JVM
-jpype.startJVM(classpath=['BabelNet-API-5.3/lib/*', 'BabelNet-API-5.3/babelnet-api-5.3.jar', 'config'])
-
-
-from it.uniroma1.lcl.babelnet import BabelNet
-from it.uniroma1.lcl.jlt.util import Language
-
-import util
-import pipeline
-import patch_lingpy
-
+import pandas as pd
 import matplotlib.pyplot as plt
-
-language_set = "lexibench"
-redo = False
-
-base_dir = os.path.join("results", language_set)
-wordlist_cognate_dir = os.path.join("resources", "lexibench_wordlists")
-plots_dir = os.path.join(base_dir, "plots")
-
-datasets = set()
-for file_name in os.listdir(wordlist_cognate_dir):
-    datasets.add(file_name.split("_")[0])
-
-all_entropies = []
-for dataset in datasets:
-    full_name = dataset + "_all"
-    wordlist_cognate_path = os.path.join(wordlist_cognate_dir, full_name + "_wordlist_cognate.tsv")
-    if not os.path.isfile(wordlist_cognate_path):
-        continue
-
-    print(full_name)
-    try:
-        cd = CognateData.from_edictor_tsv(wordlist_cognate_path)
-    except:
-        continue
-    if cd.num_taxa() < 4:
-        continue
-    all_entropies.append(cd.bin_entropy())
-
-plt.hist(all_entropies, bins = 20)
+plots_dir = "results/lexibench/plots"
+if not os.path.isdir(plots_dir):
+    os.makedirs(plots_dir)
+df = pd.read_csv("resources/lexibench_metadata.tsv", sep = "\t")
+plt.hist(df["bin_entropy"], bins = 20)
 plt.xlabel("entropy")
 plt.ylabel("num datasets")
 plt.savefig(os.path.join(plots_dir, "hist_entropies.png"))
