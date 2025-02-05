c43ed1bbaa4f6dbb4181ff3ca43051ba11b89212
Luise Haeuser <haeusele@hitssv566.villa-bosch.de>
2025-02-05 08:26:12 +0000

    Renaming in completeness analysis

--- completeness_analysis.py
+++ completeness_analysis.py
@@ -14,7 +14,7 @@ plots_dir = os.path.join("results", "completeness_plots")
 if not os.path.isdir(plots_dir):
     os.makedirs(plots_dir)
 
-for pre in ["main", "iecor"]:
+for pre in ["dense", "iecor"]:
     lexibank_path = os.path.join("results", "lexibank-analyzed_languagelists", "wordlist_cognate",  pre + "_swadesh100_wordlist_cognate.tsv")
     babelnet_path = os.path.join("results", pre, "wordlist_cognate", "swadesh100_epitran_wordlist_cognate.tsv")
     lexibank_cd = CategoricalData.from_edictor_tsv(lexibank_path)
